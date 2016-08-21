3d00937ea5b6d6cdc432aedd6eadcc10cb5d0838
Maurits van Altvorst <mvanaltvorst@icloud.com>
2016-08-21 11:16:20 +0200

    Fixed typo and optimized the program to limit RAM usage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate image;
-use  std::env;
+use std::env;
 use std::fs::File;
 
 const DIRECTIONS: [(i32, i32); 4] = [(0, -1), (-1, 0), (0, 1), (1, 0)];
 
 fn main() {
     // Get user input
-    let size: i32 = match env::args().nth(1) {
+    let mut size: i32 = match env::args().nth(1) {
         None => panic!("Didn't get size argument."),
         Some(input) => match input.parse::<i32>() {
             Err(_) => panic!("Unable to parse input."),
             Ok(parsed) => parsed
         }
     };
+    if size % 2 == 0 {
+        size = size + 1;
+        println!("Size isn't odd, new dimensions are {0}x{0}", size);
+    }
     let offset = size / 2;
 
     // Initialize grid
@@ -26,7 +30,8 @@ fn main() {
     let mut yv = 0;
     let mut direction: usize = 3;
     let mut value: u32 = 1;
-    let mut grid: Grid = Grid::new(size as usize);
+    // let mut grid: Grid = Grid::new(size as usize);
+    let mut imgbuf = image::ImageBuffer::new(size as u32, size as u32);
 
     // Fill grid
     loop {
@@ -58,63 +63,71 @@ fn main() {
             yv = DIRECTIONS[direction].1;
         }
 
-        if value as i32 >= size * size + 2 * size { // (size + 1) * (size + 1) - 1
+        if x > offset || x < (-offset) || y > offset || y < (-offset) {
             break;
+        } else {
+            // grid.set((x + offset) as usize, (y + offset) as usize, is_prime(value));
+            if is_prime(value) {
+                imgbuf.put_pixel((x + offset) as u32, (y + offset) as u32, image::Luma([0u8]))
+            }
+            else {
+                imgbuf.put_pixel((x + offset) as u32, (y + offset) as u32, image::Luma([255u8]))
+            }
         }
-
-        grid.set((x + offset) as usize, (y + offset) as usize, is_prime(value));
         value += 1;
     }
 
     // grid.print();
-    grid.save_to_image();
+    // grid.save_to_image();
+    let ref mut fout = File::create("output.png").expect("Failed to create output.png.");
+    image::ImageLuma8(imgbuf).save(fout, image::PNG).expect("Failed to save image.");
 }
 
-struct Grid {
-    values: Box<Vec<bool>>,
-    size: usize
-}
-
-impl Grid {
-    fn new(size: usize) -> Grid {
-        Grid {
-            values: Box::new(vec![false; size * size + 1]),
-            size: size
-        }
-    }
-    fn get(&self, x: usize, y: usize) -> bool {
-        self.values[x * (self.size - 1) + y]
-    }
-    fn set(&mut self, x: usize, y: usize, value: bool) {
-        self.values[x * (self.size - 1) + y] = value;
-    }
-    fn print(&self) {
-        let mut counter = 0;
-        for value in self.values.iter() {
-            print!("{}", match *value {
-                true => "#",
-                false => " "
-            });
-            if counter > self.size {
-                println!("");
-                counter = 0;
-            }
-            counter += 1;
-        }
-    }
-    fn save_to_image(&self) {
-        let mut imgbuf = image::ImageBuffer::new(self.size as u32, self.size as u32);
-        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-            if self.get(x as usize, y as usize) {
-                *pixel = image::Luma([0u8]);
-            } else {
-                *pixel = image::Luma([255u8]);
-            }
-        }
-        let ref mut fout = File::create("output.png").unwrap();
-        let _ = image::ImageLuma8(imgbuf).save(fout, image::PNG);
-    }
-}
+// struct Grid {
+//     values: Box<Vec<bool>>,
+//     size: usize
+// }
+//
+// impl Grid {
+//     fn new(size: usize) -> Grid {
+//         Grid {
+//             values: Box::new(vec![false; size * size + 1]),
+//             size: size
+//         }
+//     }
+//     fn get(&self, x: usize, y: usize) -> bool {
+//         self.values[x * (self.size - 1) + y]
+//     }
+//     fn set(&mut self, x: usize, y: usize, value: bool) {
+//         self.values[x * (self.size - 1) + y] = value;
+//     }
+//     fn print(&self) {
+//         let mut counter = 0;
+//         for value in self.values.iter() {
+//             print!("{}", match *value {
+//                 true => "#",
+//                 false => " "
+//             });
+//             if counter > self.size {
+//                 println!("");
+//                 counter = 0;
+//             }
+//             counter += 1;
+//         }
+//     }
+//     fn save_to_image(&self) {
+//         let mut imgbuf = image::ImageBuffer::new(self.size as u32, self.size as u32);
+//         for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+//             if self.get(x as usize, y as usize) {
+//                 *pixel = image::Luma([0u8]);
+//             } else {
+//                 *pixel = image::Luma([255u8]);
+//             }
+//         }
+//         let ref mut fout = File::create("output.png").unwrap();
+//         let _ = image::ImageLuma8(imgbuf).save(fout, image::PNG);
+//     }
+// }
 
 fn is_prime(n: u32) -> bool {
     if n == 2 || n == 3 {
